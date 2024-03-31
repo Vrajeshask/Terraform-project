7b084d8dc49bbb0a390b048f63af21e9515ac5d5
Vrajesh <142744675+Vrajeshask@users.noreply.github.com>
2024-03-31 18:13:29 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,7 +11,6 @@ resource "azurerm_app_service_plan" "app_service_plan" {
   name                = var.azurerm_app_service_plan
   location            = azurerm_resource_group.ncpl_group.location
   resource_group_name = azurerm_resource_group.ncpl_group.name
-  kind = "Windows"
 
   sku {
     tier = var.sku
@@ -24,18 +23,4 @@ resource "azurerm_app_service" "ncpl_app_service" {
   location            = azurerm_resource_group.ncpl_group.location
   resource_group_name = azurerm_resource_group.ncpl_group.name
   app_service_plan_id = azurerm_app_service_plan.app_service_plan.id
-
-  site_config {
-    dotnet_framework_version = "v4.0"
-  }
-
-  app_settings = {
-    "SOME_KEY" = "some-value"
-  }
-
-  connection_string {
-    name  = "Database"
-    type  = "SQLServer"
-    value = "Server=some-server.mydomain.com;Integrated Security=SSPI"
-  }
 }
